57bf24df41fe3d128020a1988e33d7d55452b98f
JsMrn0 <52720799+JsMrn0@users.noreply.github.com>
2021-01-26 23:21:20 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+const GRID_HOST = 'http://137.220.50.58:4444/wd/hub';
+//this is the host we connect to create a session
+let fs = require('fs');
+//this is for read files-create files
+
+class instagramBot {
+    // example about how to work with classess
+
+    init() {
+        this.signin()
+    }
+
+    async signin() {
+        console.log("hey, now you are working with selenium lmao");
+    }
+
+}
+
+const ib = new instagramBot();
+ib.init();
